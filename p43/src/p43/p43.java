0f9af94638b3a23fafc233819f0421ceddd22930
kk653789 <kk653789@gmail.com>
2019-10-10 20:29:17 +0800

    ch2

---
+++ p43/src/p43/p43.java
@@ -0,0 +1,14 @@
+package p43;
+
+public class p43 {
+	public static void main(String[] args) {
+		int[] test = {80, 60, 22, 50, 75};
+		
+		for(int i=0; i<test.length ;i++) {
+			System.out.println("第"+(i+1)+"個人的分數是"+test[i]+"分");
+		}
+		
+		System.out.println("考試的人數是"+test.length+"人");
+	}
+}
+//106360111
